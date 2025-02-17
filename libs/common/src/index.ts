6a7fe44fb414e599f6e561a98f64ae9c44177511
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-02-17 11:13:42 +0700

    fix: nest start error

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,5 +1,4 @@
 export * from './common.module';
 export * from './common.service';
-export * from './types/user-information';
 export * from './mail/mail.module';
 export * from './mail/mail.service';
